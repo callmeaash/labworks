b5281b4c7aa297fa02ccb1ebf36a40dae2777bfc
callmeaash <aashishbogati9@gmail.com>
2024-08-19 22:19:11 +0545

    updated lab1 problem

--- lab1/constant.c
+++ lab1/constant.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #define PI 3.14
 
+const int global = 20;
+
 int main(void)
 {
     const int CONSTANT = 10;
-    printf("Global constant PI = %.2f\n", PI);
+
+    printf("Defined PI = %.2f\n", PI);
     printf("Local constant CONSTANT = %i\n", CONSTANT);
+    printf("Global constant : %i\n", global);
 }
